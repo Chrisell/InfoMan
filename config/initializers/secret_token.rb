faa048cb225d2d455ec6ed23fdfa22ea549780da
Chris Ell <ellbot.chris@gmail.com>
2013-05-20 11:01:33 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfoMan::Application.config.secret_token = 'a8190a510c7343402867da0b6fe17416b8dbb1d35d557db300760f908421f9ee5f77a3187a43a737e49036ccdd054cec209d255f19d1a503c04373b87f0770c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+InfoMan::Application.config.session_store :cookie_store, key: '_InfoMan_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# InfoMan::Application.config.session_store :active_record_store
